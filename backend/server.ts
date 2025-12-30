d627869717521327cacbb1018c969250b96f764b
blaxcull <blaxcull@gmail.com>
2025-12-30 13:04:07 +0530

    added backend configured to render

--- backend/server.ts
+++ backend/server.ts
@@ -1,28 +1,19 @@
 import express from "express";
 import cors from "cors";
-import songsRoutes from './routes/song.routes';
-
-
+import songsRoutes from "./routes/song.routes";
 
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
 };
 
 app.use(express.json());
-
-
 app.use(cors(corsOptions));
 
-
-
-
-// generate signed url for each song and cover
-// insert into mongodb
-app.use('/api/songs', songsRoutes);
-
+app.use("/api/songs", songsRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
